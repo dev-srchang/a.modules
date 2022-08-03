269d046440937eb1f578c3acec45dae22462a7a8
ywm.srchang <srchang@ywmobile.co.kr>
2022-08-03 09:25:27 +0900

    create json project

---
+++ app/src/main/java/com/ywmobile/modules/json/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ywmobile.modules.json.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ywmobile.modules.json.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
